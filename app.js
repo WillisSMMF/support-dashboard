12597db0ed792e137b9bc74549639aeb423c6c01
WillisSMMF <willis@smmf.co.id>
2026-05-21 16:40:40 +0700

    Update app.js

--- app.js
+++ app.js
@@ -3,7 +3,7 @@
    ========================================== */
 
 // URL Google Sheet yang dipublish sebagai CSV
-const SHEET_CSV_URL = 'https://docs.google.com/spreadsheets/d/e/2PACX-1vRxmI-osn5Oq2XBN8igHn5RpcxyFlhU7E02VtUgV3CLrLjrTiG09LfaC9jvXIpPUeQgGP22IW2eT5WZ/pub?gid=408991878&single=true&output=csv';
+const SHEET_CSV_URL = 'https://script.google.com/macros/s/AKfycbxWGcFMR2xel-CBEXikFMARC3tVg1kr2lKiZTxJ0w9mPh4kC7HbRGHPBpU6d2qE8dVwOA/exec';
 
 // CORS Proxy fallback — digunakan saat membuka dari file:// lokal
 const CORS_PROXIES = [
